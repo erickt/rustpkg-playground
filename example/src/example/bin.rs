eb74f1480ddf6d3b3b1bbb0167f7981c3802d1bd
Erick Tryzelaar <erick.tryzelaar@gmail.com>
2014-01-05 11:30:10 -0500

    add example

---
+++ example/src/example/bin.rs
@@ -0,0 +1,6 @@
+extern mod simple = "github.com/erickt/rust-playground/simple";
+extern mod complex = "github.com/erickt/rust-playground/rust-complex#complex";
+extern mod multiple_foo = "github.com/erickt/rust-playground/multiple#foo";
+extern mod multiple_bar = "github.com/erickt/rust-playground/multiple#bar";
+
+fn main() {}
